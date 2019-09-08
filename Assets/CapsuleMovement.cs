666ea757e5663865cac22fd2affed0b588083beb
Ekemini <3k3m1n1@gmail.com>
2019-09-08 00:24:55 -0400

    wrote scripts for cylinder + capsule, sorry for diff version of unity

---
+++ Assets/CapsuleMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleMovement : MonoBehaviour
+{
+    public float cartwheelSpeed = -700;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NeverEndingCartwheel();
+    }
+
+    void NeverEndingCartwheel()
+    {
+        transform.Rotate(0, 0, cartwheelSpeed * Time.deltaTime);
+        transform.position += Vector3.right * Time.deltaTime;
+    }
+}
---
+++ Assets/CylinderMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SlomoElevator();
+    }
+
+    void SlomoElevator()
+    {
+        transform.position = new Vector3(transform.position.x, 2 * Mathf.Tan(Time.time), transform.position.z);
+    }
+}
