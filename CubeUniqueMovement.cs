a80cb73b55162775d59757a3715ed765cd8ef7b7
David Calixto <davidcalixto9@gmail.com>
2019-09-08 22:28:52 -0400

    Uploaded Cube's Unique Movements
    Uploaded the cube section of the homework.

---
+++ CubeUniqueMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeUniqueMovement : MonoBehaviour
+{
+    int randomPositionThatIsEitherLeftOrRight;
+    int randomPositionThatIsEitherupOrDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        randomPositionThatIsEitherLeftOrRight = Random.Range(-5, 5);
+        randomPositionThatIsEitherupOrDown = Random.Range(0, 2);
+        transform.position = new Vector3(-10 + 20 * randomPositionThatIsEitherLeftOrRight, randomPositionThatIsEitherupOrDown, transform.position.z);
+        transform.position += Vector3.back * Time.deltaTime;
+    }
+}
